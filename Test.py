e1981fa4f8ed7bb28c13260e42bf56b850c34dbf
lucascsmenezes <77303240+lucascsmenezes@users.noreply.github.com>
2021-01-11 19:48:41 -0300

    fix bugs cadastro
    Corrigida a identação, acrescimo de espaço nos inputs, fix de chave de input, fix de chamada de função a linha 16

--- Test.py
+++ Test.py
@@ -1,22 +1,61 @@
-def principal():
+def existe_contato(lista, email):
 
-def adicionar():
-    pass
+    if len(lista) > 0:
+        for contato in lista:
+            if contato ["email"] == email:
+                return True
+
+    return False
+
+def adicionar(lista):
+
+
+    while True:
+        email = input('Digite o e-mail do contato:')
+
+        if not existe_contato(lista, email):
+            break
+        else:
+            print('Esse e-mail já foi utilizado') 
+            print('por favor utilize outro e-mail.')
+
+
+    contato = {
+        'email':  (input('Digite o email: ')),
+        'nome':  (input('Digite o nome: ')),
+        'tel': int(input('Digite o telefone: '))
+    }
+
+    lista.append(contato)
+
+    print('O contato {} foi cadastrado com sucesso!\n'.format(contato['nome']))
+
+
 
 def alterar():
     pass
 
+
+
 def excluir():
     pass
+
+
 
 def buscar():
     pass
 
+
+
 def listar():
     pass
 
-def sair():
-    pass
+
+
+def principal():
+
+    lista = [] # Inicializando a lista vazia
+
 
     while True:
         print('1 - adicionar contato')
@@ -25,10 +64,11 @@ def sair():
         print('4 - buscar contato')
         print('5 - listar contatos')
         print('6 - sair')
+        
         opção = int(input('> '))
     
         if opção == 1:
-            adicionar()
+            adicionar(lista)
         elif opção == 2:
             alterar()
         elif opção == 3:
@@ -42,6 +82,5 @@ def sair():
             break
         else:
             print('Opção Invalida')
-
 
 principal()
